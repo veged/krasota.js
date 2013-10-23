d962382b24ab0b297b3211e9107bd0c8f6a60730
Denis Payase <lostsoul@yandex-team.ru>
2013-10-23 21:08:24 +0400

    add comma list whitespace expand beautifier

---
+++ tests/comma-list-whitespaces-expand.js
@@ -0,0 +1,18 @@
+var foo1 = {a: 'a', b: 'b',c: 'c'};
+var foo2 = { a: 'a',b: 'b',c: 'c' };
+var foo3 = { a: 'a',b: 'b'   ,c: 'c' };
+var foo4 = { a: 'a',b: 'b'   /*aaaa*/,c: 'c' };
+var foo5 = {
+    a: 'a',
+    b: 'b',
+    c: 'c'
+};
+
+var bar1 = [ 1,  2,3 ];
+var bar2 = [1,2   ,3];
+var bar3 = [1,2 /*asdasdas*/,3];
+
+function(a,   b      , c) {};
+function( a,b,c ) {};
+function( a ) {};
+function(a) {};
